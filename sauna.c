8933c4df2759e6bc82c2f9470543198ab54ed6fe
Lantaros <ruileixo@hotmail.com>
2017-05-08 19:14:58 +0100

    Almost finished sauna's logic, still needs to write log files

--- sauna.c
+++ sauna.c
@@ -10,13 +10,18 @@
 #include <pthread.h>
 
 #define FIFO_PERM 0700
+void * handleRequest(void * arg);
 
+//Global variables
 int generatorFD;
 int rejectedFD;
+sem_t steamRoomSem;
 
-int main(int argc, char* argv[]) 
-    
-	if (argc != 2){
+
+
+int main(int argc, char* argv[]){
+	
+    if (argc != 2){
 		fprintf(stderr, "Wrong usage: ./sauna <max capacity>\n");
 		exit(1);
 	}
@@ -27,41 +32,65 @@ int main(int argc, char* argv[])
 		fprintf(stderr, "Invalid capacity %d\n", maxCapacity);
 		exit(2);
 	}
+	
+	sem_init(&steamRoomSem,0, maxCapacity);//Steam Room semaphore is initialized with maxCapacity
+	
 
-	//Open entrada in reading mode
+	//FIFO's
 	if ((generatorFD = open("/tmp/entrada", O_RDONLY)) == -1){
 		perror("Fail on opening entrada for reading");
 		exit(3);
 	}
 	
-	//Create rejeitados
+	char nRequests[257];
+	read(generatorFD, nRequests, size(int));
+	pthread_t* threadIDs = malloc(atoi(nRequests) * sizeof(pthread));
+	int index = 0;
+	
+
     if(mkfifo("/tmp/rejeitados", FIFO_PERM) == -1){
         perror("Error on creating FIFO rejeitados");
 		exit(4);
 	}
 
-	//Open rejeitados in writing mode
+
 	if ((rejectedFD = open("/tmp/rejeitados", O_WRONLY | O_CREAT)) == -1){
 		perror("Error on opening rejeitados for writing");
 		exit(5);
 	}
 
-	//int nRequests;
 
-	//Receive the number of total requests from the generator
-	//read(generatorFD, &nRequests, sizeof(nRequests));
-
-	pthread_t waitingLine;
-	pthread_create(&waitingLine, NULL, handleLine, );
+	Request* request;
+	read(generatorFD, &request, sizeof(Request*)
+	char currentGender = request.gender; //1st person to enter
+	
+	while(!read(generatorFD, &request, sizeof(Request*){
+		if(request->gender != currentGender){
+			if(request->++rejection_number == 3)
+				continue; //Discarded
+			
+			write(rejectedFD, &request, sizeof(Request*));
+		else{
+			currentGender = request->gender;
+			pthread_create(threadIDs[index], NULL, requestHandler, request);
+			index++;
+		}
+	}
+	
+	int existingThrds = index - 1;
+	for(index = 0; index < existingThrds, index++)
+		pthread_join(threadIDs[index], NULL);
 
 	//Missing stuff
-
+	//Free / delete stuff
 	ptread_join(waitingLine, NULL);
+	free(threadIDs);
 	unlink("/tmp/rejeitados");
 }
 
-void * handleRequests(void * arg){
-
+void * handleRequest(void * arg){
+	sem_wait(&steamRoomSem);
+	sem_post(&steamRoomSem);
 	return NULL;
 
 }
