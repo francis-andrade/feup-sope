b3fa40ccfdf512fac6c0ced82cdf829bdd93a683
francis-andrade <up201503481@fe.up.pt>
2017-05-14 04:07:37 +0100

    documentation added for utilities

--- utilities.h
+++ utilities.h
@@ -18,12 +18,17 @@ double getProcTime();
 *@brief This function transforms a string in a number with a certain width (that is with a certain number of spaces to be determined on front of it)
 *@param rs string that is going to be transformed
 *@param n number that is going to be passed to a string with a certain width
-*@param width width of the string that is going to be returned
+*@param width width of the string that is going to be passed to rs
 *@param decimal number of decimal places. They can only be 0, 1 or 2
 */
 void swidth(char * rs, double n, int width, int decimal);
 
-
+/**
+*@brief This function transforms a string in a nstring with a certain width (that is with a certain number of spaces to be determined on front of it)
+*@param rs string that is going to be transformed
+*@param str string that is going to be passed to a string with a certain width
+*@param width width of the string that is going to be passed to rs
+*/
 void strwidth(char * rs,char * str, int width);
 #endif
 
